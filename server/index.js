6beb486b34d503702864a4cb744ac96a18a38863
Vasan Arul <vasankarul@gmail.com>
2020-11-30 17:43:52 -0800

    Working React render of App component

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(express.json());
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
   res.send('Hello Calendar!')
---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App () {
+  return <div>Calendar is a go</div>
+}
+
+export default App;
