86f678978fdeb525f7a43394f85e5f688e96480e
Vasan Arul <vasankarul@gmail.com>
2020-12-01 17:39:23 -0800

    Addition of useState in Calendar component

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from "moment";
 import '../index.css';
 
 function Calendar () {
-  const calendar = [];
-  const value = moment();
+  const [calendar, setCalendar] = useState([]);
+  const [value, setValue] = useState(moment());
   const startDay = value.clone().startOf("month").startOf("week");
   const endDay = value.clone().endOf("month").endOf("week");
-  const day = startDay.clone().subtract(1, "day");
 
-  while(day.isBefore(endDay, "day")) {
-      calendar.push(
+    useEffect(() =>{
+      const day = startDay.clone().subtract(1, "day");
+      const a = [];
+
+      while(day.isBefore(endDay, "day")) {
+        a.push(
           Array(7).fill(0)
-          .map(() => day.add(1, "day").clone())
-      )
-  }
+            .map(() => day.add(1, "day").clone())
+        )
+      }
+
+      setCalendar(a);
+    }, [value])
+
   return (
     <div className="calendar">
       {
